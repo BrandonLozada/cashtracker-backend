c905934fb3bdcebdc33e0ab0ae85195157718924
BrandonLozada <brandon_lozada@outlook.com>
2025-03-19 00:03:25 -0600

    Implement database connection using Sequelize and dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 import express from 'express'
 import colors from 'colors'
 import morgan from 'morgan'
+import { db } from './config/db'
+
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log(colors.green.bold('Conexión exitosa a la BD'))
+    } catch (error) {
+        // console.log(error)
+        console.log(colors.red.bold('Falló la conexión a la BD'))
+    }
+}
+
+connectDB()
 
 const app = express()
 
