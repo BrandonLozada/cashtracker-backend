626c7ee56d89a9141c7de0a25b695a4f92a8bb1f
BrandonLozada <brandon_lozada@outlook.com>
2025-03-26 22:00:28 -0600

    Add User model and establish relationship with Budget model; update Budget model to include user reference

--- src/models/Budget.ts
+++ src/models/Budget.ts
@@ -1,5 +1,6 @@
-import { Table, Column, Model, DataType, HasMany } from 'sequelize-typescript'
+import { Table, Column, Model, DataType, HasMany, ForeignKey, BelongsTo, } from 'sequelize-typescript'
 import Expense from './Expense'
+import User from './User'
 
 @Table({
     tableName: 'budgets',
@@ -22,6 +23,12 @@ class Budget extends Model {
         onDelete: 'CASCADE',
     })
     declare expenses: Expense[]
+
+    @ForeignKey(() => User)
+    declare userId: number
+
+    @BelongsTo(() => User)
+    declare user: User
 }
 
 export default Budget
