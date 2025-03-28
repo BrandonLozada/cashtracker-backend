072e2248163f25f783991b16c9090454cdd81755
BrandonLozada <brandon_lozada@outlook.com>
2025-03-27 20:30:13 -0600

    Add account confirmation endpoint with token validation

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -7,14 +7,12 @@ import { AuthEmail } from '../emails/AuthEmail'
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
         const { email, password } = req.body
-
-        // Para evitar que se creen cuentas duplicadas
         const userExists = await User.findOne({ where: { email } })
         if (userExists) {
             const error = new Error('Ya existe una cuenta con ese correo electrónico.')
             res.status(409).send({ error: error.message })
+            return
         }
-
         try {
             const user = new User(req.body)
             user.password = await hashPassword(password)
@@ -29,5 +27,19 @@ export class AuthController {
         } catch (error) {
             res.status(500).send({ error: 'No se pudo crear la cuenta.' })
         }
+    }
+
+    static confirmAccount = async (req: Request, res: Response) => {
+        const { token } = req.body
+        const user = await User.findOne({ where: { token } })
+        if (!user) {
+            const error = new Error('Token no válido.')
+            res.status(401).send({ error: error.message })
+            return
+        }
+        user.confirmed = true
+        user.token = null
+        await user.save()
+        res.json('Cuenta confirmada correctamente.')
     }
 }
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -2,18 +2,30 @@ import { Router } from 'express'
 import { body } from 'express-validator'
 import { handleInputErrors } from '../middleware/validation'
 import { AuthController } from '../controllers/AuthController'
+import { limiter } from '../config/limiter'
 
 const router = Router()
 
-router.post('/create-account',
-    body('name')
-    .notEmpty()
-    .withMessage('El nombre no puede ir vacío.'),
-    body('password').isLength({ min: 8 })
-    .withMessage('La contraseña debe tener al menos 8 caracteres.'),
-    body('email').isEmail()
-    .withMessage('El correo electrónico no es válido.'),
+router.post(
+    '/create-account',
+    body('name').notEmpty().withMessage('El nombre no puede ir vacío.'),
+    body('password')
+        .isLength({ min: 8 })
+        .withMessage('La contraseña debe tener al menos 8 caracteres.'),
+    body('email').isEmail().withMessage('El correo electrónico no es válido.'),
     handleInputErrors,
-    AuthController.createAccount)
+    AuthController.createAccount
+)
+
+router.post(
+    '/confirm-account',
+    limiter,
+    body('token')
+        .notEmpty()
+        .isLength({ min: 6, max: 6 })
+        .withMessage('El token no válido.'),
+    handleInputErrors,
+    AuthController.confirmAccount
+)
 
 export default router
